4e8df687b4ea677cfde3374a455d0a5e73bc28cb
Mortron <cmsl4y3r@gmail.com>
2021-12-21 18:12:10 -0700

    chaining done

--- User.js
+++ User.js
@@ -7,19 +7,23 @@ class User {
 
     makeDeposit(amt) {
         this.accountBalance += amt;
+        return this;
     }
 
     makeWithdrawal(amt) {
         this.accountBalance -= amt;
+        return this;
     }
 
     displayBalance() {
         console.log(`${this.name}'s account balance is :`+ " $"+ `${this.accountBalance}`);
+        return this;
     }
 
     transferMoney(user, amt) {
         this.accountBalance -= amt;
         user.accountBalance += amt;
+        return this;
     }
 
 
@@ -28,27 +32,13 @@ class User {
 const chris = new User("Chris Morton", "chris.morton.chris@gmail.com");
 const misty = new User("Misty Morton", "marowe0000@gmail.com");
 const jiraiya = new User("Jiraiya Morton", "jmorton@gmail.com");
-chris.makeDeposit(100);
-chris.makeDeposit(50);
-chris.makeDeposit(45);
-chris.makeWithdrawal(23);
-chris.displayBalance();
+chris.makeDeposit(100).makeDeposit(50).makeDeposit(45).makeWithdrawal(23).displayBalance()
 misty.displayBalance();
 chris.transferMoney(misty, 35);
 misty.displayBalance();
 chris.displayBalance();
-misty.makeDeposit(75);
-misty.makeDeposit(45);
-misty.makeDeposit(90);
-misty.makeWithdrawal(34);
-misty.makeWithdrawal(23);
-misty.displayBalance();
-jiraiya.makeDeposit(5000);
-jiraiya.makeWithdrawal(34);
-jiraiya.makeWithdrawal(90);
-jiraiya.makeWithdrawal(78);
-jiraiya.displayBalance();
-chris.transferMoney(jiraiya, 89);
-chris.displayBalance();
+misty.makeDeposit(75).makeDeposit(45).makeDeposit(90).makeWithdrawal(34).makeWithdrawal(23).displayBalance()
+jiraiya.makeDeposit(5000).makeWithdrawal(34).makeWithdrawal(90).makeWithdrawal(78).displayBalance()
+chris.transferMoney(jiraiya, 89).displayBalance();
 jiraiya.displayBalance();
 
